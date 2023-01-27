defd7909babe76ae2a070bf303e01cfceb6d29d5
jwong65 <jason.wong6507@gmail.com>
2023-01-26 19:02:09 -0500

    Added components directory and initial component files

---
+++ src/components/SubmitForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SubmitForm() {
+  return (
+    <div>SubmitForm</div>
+  )
+}
---
+++ src/components/UserInterface.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserInterface() {
+  return (
+    <div>UserInterface</div>
+  )
+}
